b51a64c57f5320c365a0985e09430e31eb6896b0
Carlos <carlosleon0831@gmail.com>
2023-03-17 17:31:35 -0600

    course completed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
@@ -8,20 +11,19 @@ const createTask = (evento) => {
     const task = document.createElement('li');
     task.classList.add('card');
     input.value = '';
-    const content = `
-    <div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>
-    `;
-    task.innerHTML = content;
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    
+    // task.innerHTML = content;
 
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
-
-    console.log(content);
 };
 
 btn.addEventListener("click", createTask);
-
 
